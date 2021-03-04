5fbb00bebc48a6afd26b03525b380512d6bb0fa3
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-04 16:24:47 +0100

    change name of function

---
+++ functions/noah.js
@@ -0,0 +1,4 @@
+"use strict";
+import server from "../__sapper__/build/server/server";
+
+export const handler = server;
--- functions/render.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-import { server } from "../__sapper__/build/server/server";
-
-exports.handler = server;
