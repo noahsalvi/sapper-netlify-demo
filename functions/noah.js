df1ee1cf7fba3788bff29c7dec901c69cf2f2a7e
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-04 18:40:40 +0100

    add env

--- functions/noah.js
+++ functions/noah.js
@@ -1,5 +1,9 @@
 "use strict";
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+process.env.PORT = process.env.PORT || 3000;
+
+console.log("Starting server on port " + process.env.PORT);
 const serverless = require("serverless-http");
-const server = require("../__sapper__/build/server/server");
+const server = require("../__sapper__/build/server/server.js");
 
 module.exports.handler = serverless(server);
