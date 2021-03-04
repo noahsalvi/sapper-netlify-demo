c60371ceff7223fe0177e1fadd66ae98a28d59ea
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-04 16:29:19 +0100

    update

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-module.exports = express() // You can also use Express
+export default express() // You can also use Express
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
