fcb8a1e55fcd979ac2e346db74b85b5afba3eea0
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-04 17:57:34 +0100

    t

--- functions/noah/index.js
+++ functions/noah/index.js
@@ -1,7 +1,4 @@
 "use strict";
 const server = require("../../__sapper__/build/server/server");
 
-exports.handler = async function (event, context) {
-  console.log("heeeelllooo");
-  server();
-};
+exports.handler = server;
