0b233ebbd397850946b51c7462298173665df45b
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-04 17:39:21 +0100

    change path

--- functions/noah/index.js
+++ functions/noah/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const server = require("../__sapper__/build/server/server");
+const server = require("./__sapper__/build/server/server");
 
 exports.handler = async function (event, context) {
   server();
