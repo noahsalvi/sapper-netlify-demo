2459e8f90b4d8553ab00b1dadb501dc2626d6b1e
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 20:30:38 +0100

    t polka

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import sirv from "sirv";
-import express from "express";
+import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const server = express().use(compression({ threshold: 0 }));
+const server = polka().use(compression({ threshold: 0 }));
 
 if (dev) {
   server.use(sirv("static", { dev }));
