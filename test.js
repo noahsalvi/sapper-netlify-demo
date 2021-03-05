2b7969d73c4c744554ebb7d3b687500e9c993e44
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 12:03:35 +0100

    t

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ fs.readFile(fileName, "utf8", function (err, data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace("__sapper__/build", "./__sapper__/build");
+  var result =
+    "console.log(process.cwd();\n" +
+    data.replace("__sapper__/build", "./__sapper__/build");
 
   fs.writeFile(fileName, result, "utf8", function (err) {
     if (err) return console.log(err);
