ef5b6c59028bce389ffde79aa5082fc9028d035f
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 13:30:50 +0100

    test

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 var fs = require("fs");
-const fileName = "__sapper__/build/server/server.js";
+const fileName = "functions/noah/__sapper__/build/server/server.js";
 fs.readFile(fileName, "utf8", function (err, data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(`"__sapper__/build"`, `"src/__sapper__/build"`);
+  var result = data.replace(
+    `"__sapper__/build"`,
+    `__dirname + "__sapper__/build"`
+  );
 
   fs.writeFile(fileName, result, "utf8", function (err) {
     if (err) return console.log(err);
