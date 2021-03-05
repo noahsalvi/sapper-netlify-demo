74e38d77e08b7a4ecceb4de7eeeef9d7ea252379
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 10:21:29 +0100

    remoev bug

--- functions/noah.js
+++ functions/noah.js
@@ -8,13 +8,6 @@ const execSync = require("child_process").execSync;
 const output = execSync("ls", { encoding: "utf-8" }); // the default is 'buffer'
 console.log("Output was:\n", output);
 
-buf
-  .toString()
-  .split(/\n/)
-  .forEach(function (line) {
-    // do something here with each line
-  });
-
 console.log("Starting server on port " + process.env.PORT);
 const serverless = require("serverless-http");
 const server = require("../__sapper__/build/server/server");
