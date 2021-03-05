1c4d39f7413ccdeae5105be4dec3cde9ac092f81
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 15:10:40 +0100

    t

--- src/server.js
+++ src/server.js
@@ -6,13 +6,15 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const server = express() // You can also use Express
-  .use(compression({ threshold: 0 }), sapper.middleware())
-  .listen(PORT, (err) => {
+const server = express().use(
+  compression({ threshold: 0 }),
+  sapper.middleware()
+);
+
+if (dev) {
+  server.use(sirv("static", { dev })).listen(PORT, (err) => {
     if (err) console.log("error", err);
   });
-if (dev) {
-  server.use(sirv("static", { dev }));
 }
 
 module.exports = server;
