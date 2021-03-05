b935ff1c3201e0651f91d3fd48a3a4d4a108d237
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 15:07:28 +0100

    listen

--- src/server.js
+++ src/server.js
@@ -7,15 +7,12 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 const server = express() // You can also use Express
-  .use(compression({ threshold: 0 }), sapper.middleware());
-
+  .use(compression({ threshold: 0 }), sapper.middleware())
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
 if (dev) {
-  server
-    .use(sirv("static", { dev }))
-
-    .listen(PORT, (err) => {
-      if (err) console.log("error", err);
-    });
+  server.use(sirv("static", { dev }));
 }
 
 module.exports = server;
