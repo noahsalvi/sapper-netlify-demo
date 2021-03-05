e305797f81e3df19f8e639e824ddd4c637bfe099
Noah Salvi <noah.salvi@ti8m.ch>
2021-03-05 11:15:49 +0100

    print

--- functions/noah/noah.js
+++ functions/noah/noah.js
@@ -5,8 +5,12 @@ process.env.PORT = process.env.PORT || 3000;
 const build_dir = "__sapper__/build";
 const execSync = require("child_process").execSync;
 // import { execSync } from 'child_process';  // replace ^ if using ES modules
-const output = execSync("ls", { encoding: "utf-8" }); // the default is 'buffer'
-console.log("Output was:\n", output);
+let output = execSync("ls", { encoding: "utf-8" }); // the default is 'buffer'
+console.log("ls:\n", output);
+output = execSync("ls ..", { encoding: "utf-8" }); // the default is 'buffer'
+console.log("ls ..:\n", output);
+output = execSync("ls ../..", { encoding: "utf-8" }); // the default is 'buffer'
+console.log("ls ../..:\n", output);
 
 console.log("Starting server on port " + process.env.PORT);
 const serverless = require("serverless-http");
